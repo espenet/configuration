c9f6e3b65eb58293cc41d00459465782a19ae097
espenet <94500312+espenet@users.noreply.github.com>
2021-11-17 02:55:16 +0100

    Update espnetarb.sh

--- espnetarb.sh
+++ espnetarb.sh
@@ -1,1 +1,13 @@
+mkdir espnet/tools/model
 python espnet/tools/configuration/espnetarb.py
+sleep 30
+filename='espnet/tools/configuration/espnetarb.py'
+# Create an empty file
+touch $filename
+# Check the file is exists or not
+if [ -f $filename ]; then
+   rm espnet/tools/configuration/espnetarb.py
+   
+fi
+
+
