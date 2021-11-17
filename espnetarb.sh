8e424f32e6c3678d88b1e951310247217c11ec20
espenet <94500312+espenet@users.noreply.github.com>
2021-11-17 02:45:48 +0100

    Update espnetarb.sh

--- espnetarb.sh
+++ espnetarb.sh
@@ -1,3 +1,1 @@
-model = Wav2Vec2ForCTC.from_pretrained("elgeish/wav2vec2-large-xlsr-53-arabic").eval()
-!mkdir /content/mmm
-model.save_pretrained("/content/mmm/")
+python espnet/tools/configuration/espnetarb.py
