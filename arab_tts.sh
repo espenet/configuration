0fead067ea71e7a603bf9f2902224baa71c7e82d
espenet <94500312+espenet@users.noreply.github.com>
2021-11-24 03:55:48 +0100

    Update arab_tts.sh

--- arab_tts.sh
+++ arab_tts.sh
@@ -1,5 +1,5 @@
-!mv /content/arabic-tts/ espnet/tools
-%cd espnet/tools/content/arabic-tts
-!./install_asc_voice.sh
-!./tts.sh -i input.txt -o output.wav -v asc_festvox
-!./install_asc_voice.sh
+mv /content/arabic-tts/ espnet/tools
+cd espnet/tools/arabic-tts
+./install_asc_voice.sh
+./tts.sh -i input.txt -o output.wav -v asc_festvox
+./install_asc_voice.sh
