e7d5d3f2e3326f6f4b9eee189e2dd6108de457d3
espenet <94500312+espenet@users.noreply.github.com>
2022-01-11 11:56:51 +0100

    Update espnetarb.py

--- espnetarb.py
+++ espnetarb.py
@@ -1,4 +1,4 @@
-from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
+"""from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
 
 model = Wav2Vec2ForCTC.from_pretrained("elgeish/wav2vec2-large-xlsr-53-arabic").eval()
-model.save_pretrained("/content/espnet/tools/model")
+model.save_pretrained("/content/espnet/tools/model")"""
